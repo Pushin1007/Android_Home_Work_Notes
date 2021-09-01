a39d60d88cd5cad07f2a9ba98e7258f4164d9ebd
Pushin <geiby@mail.ru>
2021-09-02 01:35:51 +0300

    Initial commit

---
+++ app/src/main/java/geekbrains/android_home_work_notes/MainActivity.java
@@ -0,0 +1,14 @@
+package geekbrains.android_home_work_notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
